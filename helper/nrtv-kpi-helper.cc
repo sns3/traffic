b3dd9a45c74d00de0076313dde1de19aaab4d683
Budiarto Herman <budiarto.herman@magister.fi>
2013-12-04 22:27:48 +0200

    NRTV KPI is now mostly application level, except packet delay (IP level)

--- helper/nrtv-kpi-helper.cc
+++ helper/nrtv-kpi-helper.cc
@@ -60,6 +60,19 @@ NrtvKpiHelper::AddClient (Ptr<NrtvClient> client)
 {
   NS_LOG_FUNCTION (this << client);
 
+  Ipv4Address address = GetAddress (client->GetNode ());
+  client->TraceConnect ("Rx", AddressToString (address),
+                         MakeCallback (&NrtvKpiHelper::RxCallback, this));
+
+  ClientCounter_t counter;
+  counter.rxBytes = 0;
+  counter.rxAppLevelPackets = 0;
+  counter.rxIpLevelPackets = 0;
+  counter.sumPacketDelay = MilliSeconds (0);
+  NS_ASSERT_MSG (m_clientCounters.find (address) == m_clientCounters.end (),
+                 "Found a client with duplicate address " << address);
+  m_clientCounters[address] = counter;
+
   Ptr<Node> node = client->GetNode ();
   NS_ASSERT (node->GetObject<Ipv4L3Protocol> () != 0);
   m_flowMonitorHelper.Install (node);
@@ -116,78 +129,115 @@ NrtvKpiHelper::Print ()
 {
   NS_LOG_FUNCTION (this);
 
-  // PRINT HEADER
-
-  std::cout << " NRTV clients round-up statistics:" << std::endl;
-  std::cout << " -----------------------------------------------" << std::endl;
-  std::cout << std::setw (5)  << "#"
-            << std::setw (12) << "bytes"
-            << std::setw (12) << "kbps"
-            << std::setw (6)  << "pkets"
-            << std::setw (12) << "avg. delay" << std::endl;
-  std::cout << " -----------------------------------------------" << std::endl;
-
-  double duration = Simulator::Now ().GetSeconds ();
-  uint64_t rxBytes = 0;
-  double throughput = 0.0;
-  double delay = 0.0;
-
-  uint64_t sumRxBytes = 0;
-  uint32_t sumRxPackets = 0;
-  double sumDelay = 0.0;
+  // GET PACKET DELAY INFORMATION FROM FLOW MONITOR
 
   // in order to make sure all possibly lost packets are accounted for
   Ptr<FlowMonitor> flowMonitor = m_flowMonitorHelper.GetMonitor ();
   flowMonitor->CheckForLostPackets ();
 
-  // PRINT ONE LINE FOR EACH CLIENT
-
   Ptr<FlowClassifier> baseClassifier = m_flowMonitorHelper.GetClassifier ();
   Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier> (baseClassifier);
 
-  uint16_t numOfFlows = 0;
   std::map<FlowId, FlowMonitor::FlowStats> stats = flowMonitor->GetFlowStats ();
-  std::map<FlowId, FlowMonitor::FlowStats>::const_iterator it;
-  for (it = stats.begin (); it != stats.end (); ++it)
+  std::map<FlowId, FlowMonitor::FlowStats>::const_iterator it1;
+  for (it1 = stats.begin (); it1 != stats.end (); ++it1)
     {
-      Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (it->first);
+      Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (it1->first);
       if (t.sourceAddress == m_serverAddress) // only DL direction is considered
         {
-          numOfFlows++;
-          // uncount 40-byte TCP header
-          NS_ASSERT (it->second.rxBytes > (40 * it->second.rxPackets));
-          rxBytes = it->second.rxBytes - (40 * it->second.rxPackets);
-          throughput = static_cast<double> (rxBytes * 8) / 1000.0 / duration;
-          delay = it->second.delaySum.GetSeconds () / it->second.rxPackets;
-          std::cout << std::setw (5)  << numOfFlows
-                    << std::setw (12) << rxBytes
-                    << std::setw (12) << throughput
-                    << std::setw (6)  << it->second.rxPackets
-                    << std::setw (12) << delay << std::endl;
-          sumRxBytes += rxBytes;
-          sumRxPackets += it->second.rxPackets;
-          sumDelay += delay;
+          NS_ASSERT (m_clientCounters.find (t.destinationAddress) != m_clientCounters.end ());
+          m_clientCounters[t.destinationAddress].rxIpLevelPackets += it1->second.rxPackets;
+          m_clientCounters[t.destinationAddress].sumPacketDelay += it1->second.delaySum;
         }
+    }
+
+  // PRINT HEADER
+
+  std::cout << " NRTV clients round-up statistics:" << std::endl;
+  std::cout << " ----------------------------------------------------------------" << std::endl;
+  std::cout << std::setw (16) << "address"
+            << std::setw (12) << "bytes"
+            << std::setw (12) << "kbps"
+            << std::setw (12) << "packets"
+            << std::setw (12) << "avg. delay" << std::endl;
+  std::cout << " ----------------------------------------------------------------" << std::endl;
+
+  // PRINT ONE LINE FOR EACH CLIENT
+
+  double duration = Simulator::Now ().GetSeconds ();
+  double throughput = 0.0;
+  double avgDelaySecond = 0.0;
+
+  uint64_t sumRxBytes = 0;
+  uint32_t sumRxAppLevelPackets = 0;
+  uint32_t sumRxIpLevelPackets = 0;
+  double sumPacketDelaySecond = 0.0;
+
+  std::map<Ipv4Address, ClientCounter_t>::const_iterator it2;
+  for (it2 = m_clientCounters.begin (); it2 != m_clientCounters.end (); ++it2)
+    {
+      throughput = static_cast<double> (it2->second.rxBytes * 8) / 1000.0 / duration;
+      avgDelaySecond = it2->second.sumPacketDelay.GetSeconds () / it2->second.rxIpLevelPackets;
+      std::cout << std::setw (16) << AddressToString (it2->first)
+                << std::setw (12) << it2->second.rxBytes
+                << std::setw (12) << throughput
+                << std::setw (12) << it2->second.rxAppLevelPackets
+                << std::setw (12) << avgDelaySecond << std::endl;
+      sumRxBytes += it2->second.rxBytes;
+      sumRxAppLevelPackets += it2->second.rxAppLevelPackets;
+      sumRxIpLevelPackets += it2->second.rxIpLevelPackets;
+      sumPacketDelaySecond += it2->second.sumPacketDelay.GetSeconds ();
     }
 
   // PRINT FOOTER
 
   throughput = static_cast<double> (sumRxBytes * 8) / 1000.0 / duration;
-  delay = sumDelay / sumRxPackets;
-  std::cout << " -----------------------------------------------" << std::endl;
-  std::cout << std::setw (5) << "sum"
+  avgDelaySecond = sumPacketDelaySecond / sumRxIpLevelPackets;
+  std::cout << " ----------------------------------------------------------------" << std::endl;
+  std::cout << std::setw (16) << "sum"
             << std::setw (12) << sumRxBytes
             << std::setw (12) << throughput
-            << std::setw (6)  << sumRxPackets
-            << std::setw (12) << delay << std::endl;
-  std::cout << std::setw (5) << "avg"
-            << std::setw (12) << static_cast<double> (sumRxBytes) / numOfFlows
-            << std::setw (12) << throughput / numOfFlows
-            << std::setw (6)  << static_cast<double> (sumRxPackets) / numOfFlows
+            << std::setw (12) << sumRxAppLevelPackets
+            << std::setw (12) << avgDelaySecond << std::endl;
+  std::cout << std::setw (16) << "avg"
+            << std::setw (12) << static_cast<double> (sumRxBytes) / m_clientCounters.size ()
+            << std::setw (12) << throughput / m_clientCounters.size ()
+            << std::setw (12) << static_cast<double> (sumRxAppLevelPackets) / m_clientCounters.size ()
             << std::setw (12) << "n/a" << std::endl;
-  std::cout << " -----------------------------------------------" << std::endl;
+  std::cout << " ----------------------------------------------------------------" << std::endl;
 
 } // end of `Print()`
+
+
+void
+NrtvKpiHelper::RxCallback (std::string context, Ptr<const Packet> packet)
+{
+  Ipv4Address address (context.c_str ());
+  NS_ASSERT (m_clientCounters.find (address) != m_clientCounters.end ());
+  m_clientCounters[address].rxBytes += packet->GetSize ();
+  m_clientCounters[address].rxAppLevelPackets++;
+}
+
+
+Ipv4Address
+NrtvKpiHelper::GetAddress (Ptr<Node> node)
+{
+  Ptr<Ipv4> ipv4 = node->GetObject<Ipv4> ();
+  NS_ASSERT_MSG (ipv4 != 0,
+                 "No IPv4 object is found within node " << node);
+  /// \todo Still unclear if the hard-coded indices below will work in any possible cases.
+  Ipv4InterfaceAddress interfaceAddress = ipv4->GetAddress (1, 0);
+  return interfaceAddress.GetLocal ();
+}
+
+
+std::string
+NrtvKpiHelper::AddressToString (Ipv4Address address)
+{
+  std::ostringstream oss;
+  address.Print (oss);
+  return oss.str ();
+}
 
 
 } // end of `namespace ns3`
--- helper/nrtv-kpi-helper.h
+++ helper/nrtv-kpi-helper.h
@@ -22,9 +22,10 @@
 #ifndef NRTV_KPI_HELPER_H
 #define NRTV_KPI_HELPER_H
 
-#include <ns3/nstime.h>
-#include <ns3/application-container.h>
 #include <ns3/ptr.h>
+#include <ns3/nstime.h>
+#include <ns3/ipv4-address.h>
+#include <ns3/application-container.h>
 #include <ns3/flow-monitor-helper.h>
 
 
@@ -65,9 +66,22 @@ private:
   void AddClient (ApplicationContainer apps);
   void SetServer (Ptr<NrtvServer> server);
   void SetServer (ApplicationContainer apps);
+  void RxCallback (std::string context, Ptr<const Packet> packet);
+
+  static Ipv4Address GetAddress (Ptr<Node> node);
+  static std::string AddressToString (Ipv4Address address);
 
   FlowMonitorHelper m_flowMonitorHelper;
   Ipv4Address m_serverAddress;
+
+  struct ClientCounter_t
+  {
+    uint64_t rxBytes;
+    uint32_t rxAppLevelPackets;
+    uint32_t rxIpLevelPackets;
+    Time sumPacketDelay;
+  };
+  std::map<Ipv4Address, ClientCounter_t> m_clientCounters;
 
 }; // end of `class NrtvKpiHelper`
 
